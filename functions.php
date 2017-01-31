8c660d1ad13e6425c71e47d7d82f5c21550338b5
Jesse Tabor <richandjesse@gmail.com>
2017-01-31 14:39:16 -0500

    Resource Hints

--- functions.php
+++ functions.php
@@ -315,6 +315,27 @@ endif; // essay_fonts_url
 
 
 
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @param  array  $urls           URLs to print for resource hints.
+ * @param  string $relation_type  The relation type the URLs are printed.
+ * @return array  $urls           URLs to print for resource hints.
+ */
+function essay_resource_hints( $urls, $relation_type ) {
+    if ( wp_style_is( 'essay-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+        $urls[] = array(
+            'href' => 'https://fonts.gstatic.com',
+            'crossorigin',
+        );
+    }
+
+    return $urls;
+}
+add_filter( 'wp_resource_hints', 'essay_resource_hints', 10, 2 );
+
+
+
 if ( ! function_exists( 'essay_site_logo' ) ) :
 /**
  * Output an <img> tag of the site logo.
